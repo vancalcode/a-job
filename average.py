1a891db3739e49322e7a656581dc16b530dfc191
yans67 <yans67@163.com>
2023-05-21 10:15:15 +0800

    完成基本逻辑

--- average.py
+++ average.py
@@ -1,98 +1,61 @@
-
 '''
 Created on 2020年1月30日
 @author: HYG
 查询均线数据
 '''
+
 import pandas as pd
-import pymysql
-import concurrent.futures
+import date_util
+import email_util
 
 from sqlalchemy import create_engine
 
 engine_ts = create_engine('mysql+pymysql://root:huang2009@127.0.0.1:3306/stocks?charset=utf8&use_unicode=1')
 
-conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='huang2009', database='stocks')
 
-'''
-num: 计算出最近num天内收盘持续大于 5日线,10日线,20日线的票
+# 计算日平均线
 def higher_average_price(num):
+    sql = "SELECT trade_date FROM stock_deal_history_2023 ORDER BY trade_date DESC LIMIT 1;"
+    df = pd.read_sql_query(sql, engine_ts)
+    max_trade_date = df['trade_date'][0]
+    sql = """select b.trade_date from (SELECT trade_date from stock_deal_history_2023 
+            where trade_date <= {} 
+            group by trade_date 
+            order by trade_date 
+            desc limit {}) as b 
+            order by b.trade_date asc 
+            limit 1;""".format(max_trade_date, num)
+    df = pd.read_sql_query(sql, engine_ts)
+    min_trade_date = df['trade_date'][0]
+
+    sql = "select * from stock_deal_history_2023 where trade_date >= {} order by trade_date desc".format(min_trade_date)
+    df = pd.read_sql_query(sql, engine_ts)
     hit_ts_code = []
-    sql = """SELECT ts_code FROM stock_basic WHERE symbol > 000000 and symbol < 680000"""
-    base_df = pd.read_sql_query(sql, engine_ts)
+    # 判断 DataFrame 是否为空
+    if not df.empty:
+        # 按照 ts_code 列进行分组
+        grouped = df.groupby(['ts_code'])
+        # 遍历每个分组
+        for name, group in grouped:
+            hit = 0
+            for item in group.itertuples():
+                # print("code:{}, close:{},avg_5:{}, avg_10:{}, avg_20:{}".format(item.ts_code, item.close, item.avg_5, item.avg_10, item.avg_20))
+                if item.close >= item.avg_5 and item.close >= item.avg_10 and item.close >= item.avg_20 and item.avg_5 >= item.avg_10 >= item.avg_20:
+                    hit += 1
+                else:
+                    break
+                if hit == num:
+                    hit_ts_code.append(name)
 
-    for row in base_df.iterrows():
-        sql = "SELECT * FROM stock_deal_history_2023 WHERE ts_code = '{}' ORDER BY trade_date DESC".format(row[1]['ts_code'])
-        print(sql)
-        df = pd.read_sql_query(sql, engine_ts)
-        average_data = {'5_day_ma': df['close'].rolling(window=5).mean().dropna(),
-                        '10_day_ma': df['close'].rolling(window=10).mean().dropna(),
-                        '20_day_ma': df['close'].rolling(window=20).mean().dropna()}
 
-        for index, row in df.iterrows():
-            if index < num and row['close'] > average_data['5_day_ma'].iloc[index] \
-                    and row['close'] > average_data['10_day_ma'].iloc[index] \
-                    and row['close'] > average_data['20_day_ma'].iloc[index]:
-                hit_ts_code.append(row['ts_code'])
-
-            if index < num:
-                print("index:", index, "日期:", row['trade_date'],
-                      "5日均线:", average_data['5_day_ma'].iloc[index],
-                      "10日均线:", average_data['10_day_ma'].iloc[index],
-                      "20日均线:", average_data['20_day_ma'].iloc[index],
-                      "收盘价:", row['close'])
+    else:
+        print('result is empty')
     print("hit_ts_code: ", hit_ts_code)
-'''
-
-
-def improve_update_average():
-    sql = """SELECT ts_code FROM stock_basic WHERE (symbol > 000000 and symbol < 300000) or (symbol > 600000 and symbol < 680000)"""
-    base_df = pd.read_sql_query(sql, engine_ts)
-
-    def update_row(row, queue):
-        sql = "SELECT * FROM stock_deal_history_2023 WHERE ts_code = '{}' ORDER BY trade_date DESC".format(
-            row['ts_code'])
-        df = pd.read_sql_query(sql, engine_ts)
-
-        average_data = {'5_day_ma': df['close'].rolling(window=5).mean().dropna(),
-                        '10_day_ma': df['close'].rolling(window=10).mean().dropna(),
-                        '20_day_ma': df['close'].rolling(window=20).mean().dropna()}
-
-        update_data = []
-        for index, row in df.iterrows():
-            avg_5 = None
-            avg_10 = None
-            avg_20 = None
-            if index < len(average_data['5_day_ma']) and index < len(average_data['10_day_ma']) and index < len(average_data['20_day_ma']):
-                avg_5 = round(average_data['5_day_ma'].iloc[index], 2)
-                avg_10 = round(average_data['10_day_ma'].iloc[index], 2)
-                avg_20 = round(average_data['20_day_ma'].iloc[index], 2)
-                update_data.append((avg_5, avg_10, avg_20, row['id']))
-        if len(update_data) >= 1:
-            queue.put(update_data)
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        queue = queue.Queue()
-        futures = []
-        for _, row in base_df.iterrows():
-            futures.append(executor.submit(update_row, row, queue))
-        for future in concurrent.futures.as_completed(futures):
-            try:
-                future.result()
-            except Exception as e:
-                print(e)
-
-    cursor = conn.cursor()
-    while not queue.empty():
-        update_data = queue.get()
-        sql = "update stock_deal_history_2023 set avg_5 = %s, avg_10 = %s, avg_20 = %s where id = %s"
-        cursor.executemany(sql, update_data)
-    # 提交更改
-    conn.commit()
-    # 关闭游标和数据库连接
-    cursor.close()
-
+    if len(hit_ts_code) != 0:
+        message = '\n'.join(map(str, hit_ts_code)).replace(',', '').replace("'", "").replace("(", "").replace(")", "")
+        print(message)
+        email_util.send(message, "连续{}天收盘价都在5、10、20日均线之上".format(num))
 
 
 if __name__ == '__main__':
-    improve_update_average()
+    higher_average_price(7)
--- stock_deal_history.py
+++ stock_deal_history.py
@@ -4,24 +4,29 @@ Created on 2020年1月30日
 查询历史成交信息，写入mysql表中
 '''
 import time
+from datetime import datetime, timedelta
 import pandas as pd
 import tushare as ts
 from sqlalchemy import create_engine
+import pymysql
+import date_util
 
 engine_ts = create_engine('mysql+pymysql://root:huang2009@127.0.0.1:3306/stocks?charset=utf8&use_unicode=1')
+conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='huang2009', database='stocks')
 
 # 初始化pro接口
 pro = ts.pro_api('53e34221244c3d8fa6472df1c1c484b26516be0f7080fc270b0cc164')
 
 def write_data(df):
     res = df.to_sql('stock_deal_history_2023', engine_ts, index=False, if_exists='append', chunksize=5000, index_label='id')
-    print(res)
+    return res
 
-# 批量获取历史交易数据
-def record_detail_data():
+# 初始化时批量获取历史交易数据（后面不用执行）
+def init_detail_data():
     sql = """SELECT ts_code FROM stock_basic WHERE (symbol > 000000 and symbol < 300000) or (symbol > 600000 and symbol < 680000)"""
     df = pd.read_sql_query(sql, engine_ts)
     ts_codes = df['ts_code'].tolist()
+    count = 0
     for i in range(0, len(ts_codes),200):
         codes = ','.join(ts_codes[i:i+200])
         start_date = '20230101'
@@ -36,23 +41,36 @@ def record_detail_data():
                 }, fields=["ts_code","trade_date","open","high","low","close","pre_close","change","pct_chg","vol","amount"
                 ])
                 write_data(df)
+                count += len(df)
                 print("开始记录数据...")
                 time.sleep(0.2)
             except Exception as e:
                 print(e)
+    print("插入总数为: ",count)
 
-# 每天下午4点半获取收盘数据并入库
+# 每天下午4点半获取收盘数据并入库（定时任务）
 def refresh_deal_history():
-    sql = """SELECT ts_code FROM stock_basic"""
+    sql = "SELECT trade_date FROM stock_deal_history_2023 ORDER BY trade_date DESC LIMIT 1"
+    df = pd.read_sql_query(sql, engine_ts)
+    date = datetime.strptime(df['trade_date'][0], '%Y%m%d')
+
+    next_day = date + timedelta(days=1)
+    start_day = next_day.strftime('%Y%m%d')
+    today = time.strftime('%Y%m%d',time.localtime(time.time()))
+    if start_day > today:
+        start_day = today
+    
+    print("开始更新最新数据...start_day:{},today:{}".format(start_day,today))
+    
+    sql = """SELECT ts_code FROM stock_basic WHERE (symbol > 000000 and symbol < 300000) or (symbol > 600000 and symbol < 680000)"""
     df = pd.read_sql_query(sql, engine_ts)
     ts_codes = df['ts_code'].tolist()
     for i in range(0, len(ts_codes), 500):
         codes = ','.join(ts_codes[i:i+500])   
-        today = time.strftime('%Y%m%d',time.localtime(time.time()))
         df = pro.daily(**{
             "ts_code": codes,
             "trade_date": "",
-            "start_date": today,
+            "start_date": start_day,
             "end_date": today,
             "offset": "",
             "limit": ""
@@ -68,12 +86,62 @@ def refresh_deal_history():
             "pct_chg",
             "vol",
             "amount"
-        ])   
-        write_data(df)
-        print("开始记录数据...")
-        time.sleep(0.3)
+        ])
+        res = write_data(df)
+        print("开始记录最新数据...:{}".format(res))
+    print("记录完毕，准备计算日平均数...start_day:{}",start_day)    
+    record_average(start_day)
 
-if __name__ == '__main__':
+# 计算日平均数
+def record_average(start_day):
+    print("开始计算日平均数...start_day:{}",start_day)    
+    # 先不计算科创板
+    sql = """SELECT ts_code FROM stock_basic WHERE (symbol > 000000 and symbol < 300000) or (symbol > 600000 and symbol < 680000)"""
+    base_df = pd.read_sql_query(sql, engine_ts)
+    intervel_day = date_util.get_intervel_day(start_day)
+
+    for _, base_row in base_df.iterrows():
+        # 每次取只要取几十条数据即可
+        sql = "SELECT * FROM stock_deal_history_2023 WHERE ts_code = '{}' ORDER BY trade_date DESC limit {}".format(base_row['ts_code'], intervel_day + 20)
+        df = pd.read_sql_query(sql, engine_ts)
+
+        average_data = {'5_day_ma': df['close'].rolling(window=5).mean().dropna(),
+                        '10_day_ma': df['close'].rolling(window=10).mean().dropna(),
+                        '20_day_ma': df['close'].rolling(window=20).mean().dropna()}
+
+        update_data = []
+        for index, row in df.iterrows():
+            if datetime.strptime(row['trade_date'], '%Y%m%d') < datetime.strptime(start_day, '%Y%m%d'):
+                continue
+            avg_5 = None
+            avg_10 = None
+            avg_20 = None
+            print("row:",row)
+            if index < len(average_data['20_day_ma']) and index < len(average_data['10_day_ma']) and index < len(average_data['5_day_ma']):
+                if row['avg_5'] == 0:
+                    avg_5 = round(average_data['5_day_ma'].iloc[index], 2)
+                if row['avg_10'] == 0:
+                    avg_10 = round(average_data['10_day_ma'].iloc[index], 2)
+                if row['avg_20'] == 0:
+                    avg_20 = round(average_data['20_day_ma'].iloc[index], 2)
+                
+                update_data.append((avg_5, avg_10, avg_20, row['id']))
+        
+        if len(update_data) >= 1:
+            cursor = conn.cursor()
+            sql = "update stock_deal_history_2023 set avg_5 = %s, avg_10 = %s, avg_20 = %s where id = %s"
+            res = cursor.executemany(sql, update_data)          
+            conn.commit()
+            cursor.close()   
+            print("==== sql 执行内容:{},res:{}".format(update_data,res))
+    print("更新日平均数完毕...关闭数据库链接...")
+    conn.close()
+
+
+
+# if __name__ == '__main__':
+    # caculate_average('20230516')
+    # refresh_deal_history()
     # write_data(df)
-    record_detail_data()
+    # record_detail_data()
     # print(get_ts_codes())
